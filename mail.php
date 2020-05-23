474b0183fbe193ae8dee6334f4af5175bd1970e9
Verni Tapang <verni.shonidanbo@gmail.com>
2020-05-23 16:01:15 +0800

    contact-form debug commit

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $subject = $_POST['subject'];
 $content="From: $name \n Email: $email \n Message: $message";
-$recipient = "vernitapang@gmail.com";
+$recipient = "verni.tapang@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo "Email sent!";
