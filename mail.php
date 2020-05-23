1f3dcad0ab9a527d023f387c9f4a11b08b8dbe0c
Verni Tapang <verni.shonidanbo@gmail.com>
2020-05-23 16:28:47 +0800

    contact-form debug commit4

--- mail.php
+++ mail.php
@@ -6,6 +6,8 @@ $subject = $_POST['subject'];
 $content="From: $name \n Email: $email \n Message: $message";
 $recipient = "verni.tapang@gmail.com";
 $mailheader = "From: $email \r\n";
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+$errorMessage = 'There was an error while submitting the form. Please try again later';
 
 try {
     $emailText = "You have new message from contact form\n=============================\n";
